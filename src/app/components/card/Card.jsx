1afb1b7e0e7b7febe434c44f37fcb5c26d78b06a
01joanna <joa.cuevas9@gmail.com>
2024-01-11 15:24:05 +0100

    created folder structure

---
+++ src/app/components/card/Card.jsx
@@ -0,0 +1,6 @@
+
+export default function Card() {
+    return (
+        <h1>Hola mundo</h1>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+import Card from "./components/card/Card";
+
+export default function Home() {
+  return (
+    <Card />
+  )
+}
