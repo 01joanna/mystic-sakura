fd2550bbe8fc0d54d41d935b4619ad331b4fdbc8
annaLucian <garielapoves01@gmail.com>
2024-01-14 22:13:50 +0100

    feat: :rocket: the button component receives props

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,15 @@ import Button from "./components/button/Button";
 
 export default function Home() {
   return (
-    <Button target="_blank" text='Text'/>
-  )
+    <div className="flex gap-6 pt-5">
+      <Button
+        text="Ver significado"
+        sourceIcon="/assets/images/btn-icon-blue.svg"
+      />
+      <Button
+        text="Volver a tirar"
+        sourceIcon="/assets/images/btn-icon-pink.svg"
+      />
+    </div>
+  );
 }
