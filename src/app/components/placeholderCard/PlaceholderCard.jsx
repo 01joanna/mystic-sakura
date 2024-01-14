19ce8d04f79fcd8f201870577421a2c7ba5f6c96
debora-smb <debora.smborges@gmail.com>
2024-01-14 20:13:12 +0100

    feat: create placeholder card component structure

--- src/app/components/placeholderCard/PlaceholderCard.jsx
+++ src/app/components/placeholderCard/PlaceholderCard.jsx
@@ -1,5 +1,26 @@
-export default function PlaceholderCard() {
+import Image from "next/image";
+import PlaceHolderImage from "../../../../public/assets/img/placeholder-image.png";
+
+export default function PlaceholderCard({ time }) {
+
     return (
-        
-    )
+        <article>
+            <div>
+                <div>
+                    <Image
+                        src={PlaceHolderImage}
+                        alt='Círculo mágico de Sakura'
+                        width={69}
+                        height={72}
+                    />
+                    <p>
+                        Elige una carta
+                    </p>
+                    <h3>
+                        {time}
+                    </h3>
+                </div>
+            </div>
+        </article>
+    );
 }
