27169efcf6a9cadb7722d6e555549ce6fc014810
GreciaLH <glandazuriherrera@gmail.com>
2024-01-15 08:55:08 +0100

    feat: position cardlist

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,20 @@
-import Button from "./components/button/Button";
+import Card from "./components/card/Card";
+import CardList from "./components/cardList/CardList";
 
 export default function Home() {
+  const sampleCard = {
+    id: "1",
+    sakuraCard: "url-de-la-imagen",
+    englishName: "The Windy",
+    meaning: "Significado de la carta"
+};
+
   return (
-    <div className="flex gap-6 pt-5">
-      <Button
-        text="Ver significado"
-        sourceIcon="/assets/images/btn-icon-blue.svg"
-      />
-      <Button
-        text="Volver a tirar"
-        sourceIcon="/assets/images/btn-icon-pink.svg"
-      />
-    </div>
-  );
+    <div className="container mx-auto p-4">
+            <div className="my-4">
+                <Card card={sampleCard} />
+            </div>
+            <CardList />
+        </div>
+  )
 }
