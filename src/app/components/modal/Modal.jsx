0ba9ce268db37f09b6de007907c3617c07beddd6
01joanna <joa.cuevas9@gmail.com>
2024-01-17 16:30:46 +0100

    fix: changed structure on modal so it can be used based on different information

--- src/app/components/modal/Modal.jsx
+++ src/app/components/modal/Modal.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 import './styles.css'
 
-import ModalContent from "./ModalContent"
+import ModalReading from "../modalReading/ModalReading"
 
-export default function Modal() {
+export default function Modal({ children }) {
     return (
         <section className="relative flex flex-row items-center justify-center pb-10 lg:justify-between lg:border-s">
             <Image
@@ -13,18 +13,18 @@ export default function Modal() {
             height={266}
             className="flex-none absolute left-20 top-4 z-10 hidden lg:flex"
             />
+
             <div className="bg-purpleDark h-[55rem] w-[21.4rem] flex flex-col justify-around items-center rounded-3xl lg:w-[53rem] lg:h-[35.5rem]">
                 <Image src={"/assets/images/adorno-horizontal.png"} alt="Imagen de adorno horizontal, arriba" width={324} height={22} className={`${"horizontal-top"}  lg:w-[52.8rem] lg:h-[3.6rem] top-1`}/>
                 <section className="flex flex-col w-full h-full overflow-y-scroll items-center justify-center -mt-20 lg:mt-0">
-                    <ModalContent />
-                    <ModalContent reverse/>
-                    <ModalContent />
+            {children}
                 </section>
                 <Image src={"/assets/images/adorno-horizontal.png"} alt="Imagen de adorno horizontal, abajo" width={324} height={22} className={`${"horizontal-bottom"} lg:w-[52.8rem] lg:h-[3.6rem]`}/>
                 <Image src={"/assets/images/adorno-vertical.png"}  alt="Imagen de adorno horizontal, izquierda" width={355} height={57} className={`${"vertical-left"} hidden lg:flex`}/>
                 <Image src={"/assets/images/adorno-vertical.png"}  alt="Imagen de adorno horizontal, derecha" width={355} height={57} className={`${"vertical-right"} hidden lg:flex`}/>
                 {/* <Button /> */}
             </div>
+            
             <Image
             src={"/assets/images/wings-right.png"}
             alt="Imagen del ala derecho"
