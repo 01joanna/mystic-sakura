13aa71ae083c17f0718a082249cead7b3f00c2a8
debora-smb <debora.smborges@gmail.com>
2024-01-21 20:55:25 +0100

    fix:🚀 fix modal height in desktop version

--- src/app/components/modal/Modal.jsx
+++ src/app/components/modal/Modal.jsx
@@ -14,17 +14,17 @@ export default function Modal({ children }) {
             className="flex-none absolute right-[460px] top-14 z-10 hidden lg:flex"
             />
 
-            <div className="bg-purpleDark h-full w-[21.4rem] flex flex-col justify-around items-center rounded-3xl lg:w-[53rem] lg:h-[40rem]">
+            <div className="bg-purpleDark h-full w-[21.4rem] flex flex-col justify-center items-center rounded-3xl lg:w-[53rem] lg:h-[40rem]">
                 <Image src={"/assets/images/adorno-horizontal.png"} alt="Imagen de adorno horizontal, arriba" width={324} height={22} className={`${"horizontal-top"} mt-2 lg:w-[52.8rem] lg:h-[3.6rem] lg:mb-8`}/>
-                <section className="flex flex-col w-full h-full overflow-y-scroll items-center justify-center lg:justify-start lg:gap-32 py-10">
+                <section className="flex flex-col w-full h-full lg:overflow-y-scroll items-center justify-center lg:justify-start lg:gap-32 lg:py-10">
                     <ModalReading />
                     <ModalReading reverse />
                     <ModalReading />
                 </section>
                 <Image src={"/assets/images/icon-arrow-down.svg"} alt="Icon scroll down" width={30} height={30} className={`hidden lg:flex absolute bottom-20`}/>
                 <Image src={"/assets/images/adorno-horizontal.png"} alt="Imagen de adorno horizontal, abajo" width={324} height={22} className={`${"horizontal-bottom"} mb-2 lg:w-[52.8rem] lg:h-[3.6rem] lg:mt-8`}/>
-                <Image src={"/assets/images/modal-vertical.png"}  alt="Imagen de adorno horizontal, izquierda" width={355} height={57} className={`hidden lg:flex rotate-90 absolute bottom-65 right-[335px]`}/>
-                <Image src={"/assets/images/modal-vertical.png"}  alt="Imagen de adorno horizontal, derecha" width={355} height={57} className={`hidden lg:flex rotate-90 absolute bottom-65 left-[335px]`}/>
+                <Image src={"/assets/images/modal-vertical.png"}  alt="Imagen de adorno vertical, izquierda" width={380} height={57} className={`hidden lg:flex rotate-90 absolute bottom-65 right-[325px]`}/>
+                <Image src={"/assets/images/modal-vertical.png"}  alt="Imagen de adorno vertical, derecha" width={380} height={57} className={`hidden lg:flex rotate-90 absolute bottom-65 left-[325px]`}/>
                 
             </div>
             <Image
