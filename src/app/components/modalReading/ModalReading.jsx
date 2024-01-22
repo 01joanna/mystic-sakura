e45eaebfac248678f609dcff0f88513b15e70ae1
01joanna <joa.cuevas9@gmail.com>
2024-01-22 09:45:16 +0100

    feat: changed lorem ipsum to fetch api

--- src/app/components/modalReading/ModalReading.jsx
+++ src/app/components/modalReading/ModalReading.jsx
@@ -10,8 +10,8 @@ export default function ModalReading({reverse}) {
         <section className={`${containerStyle} flex flex-row relative z-0`}>
             <figure>
                 <Image
-                src={"/assets/images/Salto-Sakura.jpg"}
-                alt="Imagen de la carta correspondiente"
+                src={card.sakuraCard}
+                alt={`Imagen de la carta ${card.spanishName}`}
                 width={86}
                 height={223}
                 objectFit="cover"
@@ -20,8 +20,8 @@ export default function ModalReading({reverse}) {
         </figure>
             <aside className="flex flex-col items-center justify-center">
                 {/* cambiar el color del titulo cuando consiga saber el porque no recoge purplecolor */}
-                <h3 className=" text-yellowColor lg:text-purpleColor text-justify text-base font-showcard lg:text-5xl">El Salto</h3>
-                <p className="text-pureWhite text-sm text-justify m-6 font-jost lg:text-3xl">Representa la evasión de los problemas.</p>
+                <h3 className=" text-yellowColor lg:text-purpleColor text-justify text-base font-showcard lg:text-5xl">{card.spanishName}</h3>
+                <p className="text-pureWhite text-sm text-justify m-6 font-jost lg:text-3xl">{card.meaning}</p>
             </aside>
         </section>
         </div>
