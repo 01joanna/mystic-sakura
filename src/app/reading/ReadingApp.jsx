21f7693d7e6137bb080a9400967b46ab91e0c919
HemiCastillo <kirabley.hca@gmail.com>
2024-01-22 13:57:42 +0100

    CardPrediction add

--- src/app/reading/ReadingApp.jsx
+++ src/app/reading/ReadingApp.jsx
@@ -1,42 +1,45 @@
 "use client";
 import Kero from "@/app/components/animation/kero/Kero";
-import Star from "@/app/components/animation/star/Star";
-import PlaceholderCardGroup from "@/app/components/placeholderCardGroup/PlaceholderCradGroup";
-import ModalInfo from "@/app/components/modalInfo/ModalInfo";
 import Button from "@/app/components/button/Button";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
 import { SakuraContext } from "@/app/context";
 import { useContext } from "react";
 import { useRouter } from "next/navigation";
+import CardPrediction from "@/app/components/cardPredition/CardPredition";
 import Header from "@/app/components/header/Header";
 import styles from "@/app/reading/styles.module.css";
 import clsx from "clsx";
 
 export default function ReadingApp() {
   const {
-    isAllCardsRevealed,
-    isOpenModal,
-    handleCloseModal,
-    selectedItemsLength,
+    selectedItems,
   } = useContext(SakuraContext);
+
+  const pastCardPrediction = selectedItems.find(
+    (card)=> card.predictionTime === "pasado"
+  );
+  const presentCardPrediction = selectedItems.find(
+    (card)=> card.predictionTime === "present"
+  )
+  const futureCardPrediction = selectedItems.find(
+    (card)=> card.predictionTime === "futuro"
+  );
+
   const router = useRouter();
+
   return (
     <>
-      <Star />
       <Header />
       <Kero />
-      <DndProvider backend={HTML5Backend}>
-        <PlaceholderCardGroup />
-      </DndProvider>
-      <div
-        className={clsx(
-          !(selectedItemsLength === 3) && styles.btnReading,
-          "flex justify-center mt-[8rem] mb-[6rem] lg:mt-[11rem] lg:mb-[8rem] min-[1400px]:mt-[19rem]  min-[1400px]:m-b[4rem] min-[1600px]:mt-[4rem]"
-        )}
-      >
+      <div className={clsx("cartas_style", styles["reading-page"])}>     
+       <CardPrediction cardData={pastCardPrediction} />
+       <CardPrediction cardData={presentCardPrediction} />
+       <CardPrediction cardData={futureCardPrediction} />
+    </div>
+    <div className={clsx(
+          "flex justify-center px-[8rem] mt-[8rem] mb-[6rem] lg:mt-[11rem] lg:mb-[8rem] min-[1400px]:gap-[8.15rem]   min-[1400px]:mt-[19rem]  min-[1400px]:m-b[4rem] min-[1600px]:mt-[4rem]"
+        )}>
+
         <Button
-          disabled={!(selectedItemsLength === 3)}
           onClick={() => {
             router.push("/src/app/components/modalInfo");
           }}
@@ -45,14 +48,12 @@ export default function ReadingApp() {
         />
 
         <Button
-          disabled={!(selectedItemsLength === 3)}
           onClick={() => {
-            router.push("/src/app/components/modalInfo");
+            router.push("/home");
           }}
           text={"VOLVER A TIRAR"}
           sourceIcon={"/assets/images/btn-icon-pink.svg"}
         />
-
       </div>
     </>
   );
