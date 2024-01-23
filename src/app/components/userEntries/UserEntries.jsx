45637d30646d8a5678c79f7ffd3fea064e418edf
01joanna <joa.cuevas9@gmail.com>
2024-01-23 15:37:18 +0100

    feat: first structure of user log

--- src/app/components/userEntries/UserEntries.jsx
+++ src/app/components/userEntries/UserEntries.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
 
 function UserEntries() {
     return (
-    <table>
+    <table className="text-yellowColor">
         <tr>
+            <th></th>
             <th>Fecha</th>
             <th>Usuario</th>
             <th>Lectura</th>
+            <th></th>
         </tr>
         <tr>
-            <td>Icono</td>
+            <td>
+                <Image
+                src={"/assets/images/user-icons/kero-face.png"}
+                alt="Icono de Kero para plantilla de usuario"
+                width={10}
+                height={10}
+
+                />
+            </td>
             <td>Fecha</td>
             <td>Nombre</td>
             <td>Registro</td>
-            <td>Icono</td>
+            <td>
+                <button>
+                    <Image
+                    src={"/assets/images/user-icons/trash-icon.png"}
+                    alt="Icono borrador para plantilla de usuario"
+                    width={10}
+                    height={10}
+                    />
+                </button>
+            </td>
         </tr>
     </table>
     )
